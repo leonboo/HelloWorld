00486cd5e1eb621747ce0414be161efdac8b0594
Leon Boo <leonboo1011@gmail.com>
2014-01-07 14:27:52 +0800

    Add

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(int argc, char ** argv)
+{
+    printf("---------------------\n");
+    printf("Hello World! Leon Boo! \n");
+    printf("---------------------\n");
+
+}
